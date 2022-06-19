3f2134c2c1b96315c8cdb31fe6f47bbd7af7c6f3
christopherchinia <chinia214@gmail.com>
2022-06-19 11:07:01 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,25 +1,62 @@
+let counter = document.getElementById('counter');
 let paused = false;
 setInterval(() => {
-  if (!paused) {
-    counter.innerHTML++
-  }
-}, 1000);
+    if (!paused) {
+        counter.innerHTML++
+    
+    }
+},1000);
+
 const togglePaused = () => (paused = !paused);
-pause.addEventListener('click', togglePaused);
-// the plus button functionality
-const plusButton = document.getElementById('plus');
-plus.addEventListener('click',() =>{
-    counter.innerHTML++
-})
-// the minus button functionality
 
-const minusButton = document.getElementById('minus');
-minus.addEventListener('click',() => {
-    counter.textContent--;
-})
+const decrement = document.getElementById('minus');
+minus.addEventListener('click', () => {
+    counter.innerHTML--;
+});
 
-// heart button functionality
-const heartButton = document.getElementById('heart');
-heartButton.addEventListener('click',() => {
-    counter.textContent = `i have been liked ${counter.textContent} times`;
-})
+const increment = document.getElementById('plus');
+plus.addEventListener('click', () => {
+    counter.innerHTML++;
+});
+
+const like = document.getElementById('heart');
+like.addEventListener('click', () => {
+    let likes = 0;
+    let likeList = document.querySelector('.likes');
+    let list = document.createElement('li');
+    list.textContent = `${counter.innerHTML} has ${likes} likes`;
+    likeList.appendChild(list);
+});
+
+const pause = document.getElementById('pause');
+pause.addEventListener('click', () => {
+    if (pause.innerText === 'pause') {
+        like.disabled = true;
+        increment.disabled = true;
+        decrement.disabled = true;
+        // clearTimeout();
+        togglePaused();
+        pause.innerText = 'resume';
+    }else if ( pause.innerText === 'resume') {
+         like.disabled = false;
+        increment.disabled = false;
+        decrement.disabled = false;
+        togglePaused();
+        pause.innerText = 'pause'
+    }
+});
+
+let form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addComment();
+  form.reset();
+});
+
+const addComment = () => {
+  let input = document.getElementById('comment-input').value;
+  let userComments = document.getElementById('list');
+let comment = document.createElement('li');
+  comment.innerText = input;
+  userComments.appendChild(comment);
+};
